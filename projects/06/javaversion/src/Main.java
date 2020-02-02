1fe2ae6909220aea1f6fb98ae677b517f45cd24a
workeramo <workeramo@icloud.com>
2020-02-02 12:30:30 +0800

    Bug fixed
    完成了 rect.asm汇编器执行结果错误的BUG。
    错误原因是没有处理A指令后数字为0的符号处理。
    又遇到新的问题。测试pong这样复杂程序的时候，输出过多空行。

--- projects/06/javaversion/src/Main.java
+++ projects/06/javaversion/src/Main.java
@@ -1,10 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -15,7 +9,12 @@ public class Main {
         Parser parser = new Parser();
         ArrayList commandsList;
 
-        File file = new File("/Users/workeramo/LocalFolders/我的工作间/计算机基础资料/计算机组成原理/Coursera/nandgithubdemo/CS-Nand2Tetris/projects/06/javaversion/src/Rect.asm");
+        //File file = new File("/Users/workeramo/LocalFolders/我的工作间/MyKnowledge/CS-Nand2Tetris/projects/06/javaversion/src/Rect.asm");
+        //File outputFile = new File("/Users/workeramo/LocalFolders/我的工作间/MyKnowledge/CS-Nand2Tetris/projects/06/javaversion/src/Rect.hack"); // Test passed.
+
+        File file = new File("/Users/workeramo/LocalFolders/我的工作间/MyKnowledge/CS-Nand2Tetris/projects/06/pong/Pong.asm");
+        File outputFile = new File("/Users/workeramo/LocalFolders/我的工作间/MyKnowledge/CS-Nand2Tetris/projects/06/pong/PongMyOut.hack");
+
         if (!file.exists()) {
             System.out.println("文件不存在!");
             return ;
@@ -37,20 +36,31 @@ public class Main {
                         currentCommand = currentCommand.replaceAll("\\)","");
                     }
                     // 坑：这里很坑很坑的是PC作为地址的计数器，你在处理不止一个的自定义 symbol 的时候，之前的序列是不能算进来的。所以之前的(XXX)得删除来计算PC
+                    // 注意点：SymbolTable我第一次写的时候创建了2个，一定记得要保持是在一个里面。
                     parser.table.addEntry(currentCommand, commandPC);
                 }else {
                     commandPC +=1;
                 }
             }
 
-            // 第二步：正式翻译为二进制
+            // 第二步：正式翻译为二进制并写入文件
+
+            OutputStreamWriter writer = null;
+            try {
+                writer = new OutputStreamWriter(new FileOutputStream(outputFile), "utf-8");
+            }catch (UnsupportedEncodingException e){
+                e.printStackTrace();
+            }
+            BufferedWriter bw = new BufferedWriter(writer);
             for (int index=0; index<commandsList.size(); index++) {
                 String currentCommand = commandsList.get(index).toString();
-                System.out.println(currentCommand);
-                System.out.println(parser.getBinaryCode(currentCommand));
-
+                //System.out.println(currentCommand);
+                System.out.println();
+                String binaryCodeLine = parser.getBinaryCode(currentCommand);
+                bw.write(binaryCodeLine);
+                bw.newLine();
             }
-
+            bw.close();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
